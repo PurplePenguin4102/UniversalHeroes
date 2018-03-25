ef51f2d5dd3c99f714296211ac606c806c2d8f69
Joey Ray <joseph.h.ray@gmail.com>
2018-03-25 23:07:27 +1000

    tryne get things rendering

---
+++ UniversalHeroes/BorderBezierConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+
+namespace UniversalHeroes
+{
+    public class BorderBezierConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var border = (double)value;
+            if (border == 0)
+                return new Point(0, 0);
+            return new Point(border, 700);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
